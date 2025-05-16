f7a50145e06f94656ba6b89bd39e2694e7c4d99a
aToled <alonso.toledo75@gmail.com>
2025-05-16 16:36:35 -0400

    Implementacion de creacion de notas y arraylist de notas

--- src/main/java/org/example/Nota.java
+++ src/main/java/org/example/Nota.java
@@ -1,14 +1,25 @@
 package org.example;
 
-public class Nota {
-    private String contenido;
+import java.util.ArrayList;
 
-    public String getContenido() {
-        return contenido;
+public class Nota {
+    private final String contenido;
+    private final int numero_nota;
+    private final ArrayList<Nota> arreglo_de_notas = new ArrayList<>();
+    public Nota(int num, String contenido){
+        this.numero_nota =num;
+        this.contenido=contenido;
+    }
+
+    public void addNota(Nota nota){
+        arreglo_de_notas.add(nota);
+    }
+    public Nota getNota(int i){
+        return arreglo_de_notas.get(i);
     }
 
     @Override
     public String toString() {
-        return "Nota: " + contenido;
+        return "Nota "+ numero_nota+1 +": " + contenido;
     }
 }
