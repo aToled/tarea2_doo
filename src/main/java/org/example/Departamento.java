fb6e079b91a69848188374815df974d3f0951a90
kh12v <154763602+kh12v@users.noreply.github.com>
2025-05-16 16:27:38 -0400

    añadir empleados a departamento y registrarlos en departamento

--- src/main/java/org/example/Departamento.java
+++ src/main/java/org/example/Departamento.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Departamento implements Invitable {
+    private ArrayList<Empleado> empleados = new ArrayList<Empleado>();
     private String nombre;
     // TODO: Obtener número de empleados
-    public int obtenerCantidadEmpleados() { return 0; }
+    public int obtenerCantidadEmpleados() { return empleados.size(); }
 
     public String getNombre() {
         return nombre;
+    }
+
+    public void agregarEmpleado(Empleado empleado) {
+        empleados.add(empleado);
     }
 
     @Override
--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,11 +1,14 @@
 package org.example;
 
 public class Empleado implements Invitable {
+    private final Departamento departamento;
     private final String id;
     private final String apellidos;
     private final String nombre;
     private final String correo;
-    public Empleado(String id, String apellidos, String nombre, String correo){
+    public Empleado(Departamento departamento, String id, String apellidos, String nombre, String correo){
+        this.departamento = departamento;
+        departamento.agregarEmpleado(this);
         this.id=id;
         this.apellidos=apellidos;
         this.nombre=nombre;
@@ -27,6 +30,9 @@ public class Empleado implements Invitable {
     public String getCorreo() {
         return correo;
     }
+
+    public Departamento getDepartamento() { return departamento; }
+
     @Override
     public String toString(){
         return "Empleado: "+nombre+" "+apellidos+", ID: "+id+",Correo: "+correo;
