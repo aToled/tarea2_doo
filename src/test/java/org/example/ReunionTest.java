3aa18fb82937386597963803d2c3fdbc22d69485
kh12v <154763602+kh12v@users.noreply.github.com>
2025-05-16 21:01:37 -0400

    reunionTest

---
+++ src/test/java/org/example/ReunionTest.java
@@ -0,0 +1,129 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.datatype.Duration;
+import java.sql.Date;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReunionTest {
+
+    @Test
+    void crearReunion() {
+        Reunion reunion = new ReunionPresencial(new Date(1000000), Instant.now(), TipoReunion.MARKETING);
+
+        reunion.iniciar();
+
+        reunion.invitar(new Empleado("empleado1", "Villalobos", "Khristian", "kvillalobos2024@udec.cl"));
+        reunion.invitar(new Empleado("empleado2", "Toledo", "Alonso", "AToledo2024@udec.cl"));
+
+        reunion.agregarNota(new Nota(1, "Objetivos"));
+        reunion.agregarNota(new Nota(2, "Desafios"));
+
+        reunion.finalizar();
+
+        reunion.elaborarInforme("Sala A3");
+    }
+
+    @Test
+    void obtenerAsistencias() {
+    }
+
+    @Test
+    void obtenerAusencias() {
+    }
+
+    @Test
+    void obtenerRetrasos() {
+    }
+
+    @Test
+    void obtenerNotas() {
+    }
+
+    @Test
+    void obtenerTotalAsistencia() {
+    }
+
+    @Test
+    void obtenerTotalAusencias() {
+    }
+
+    @Test
+    void obtenerTotalRetrasos() {
+    }
+
+    @Test
+    void obtenerTotalNotas() {
+    }
+
+    @Test
+    void obtenerPorcentajeAsistencia() {
+    }
+
+    @Test
+    void agregarNota() {
+    }
+
+    @Test
+    void quitarNota() {
+    }
+
+    @Test
+    void calcularTiempoReal() {
+    }
+
+    @Test
+    void iniciar() {
+    }
+
+    @Test
+    void finalizar() {
+    }
+
+    @Test
+    void getFecha() {
+    }
+
+    @Test
+    void getHoraPrevista() {
+    }
+
+    @Test
+    void getDuracionPrevista() {
+    }
+
+    @Test
+    void getHorainicio() {
+    }
+
+    @Test
+    void getHoraFin() {
+    }
+
+    @Test
+    void getTipoDeReunion() {
+    }
+
+    @Test
+    void getModalidad() {
+    }
+
+    @Test
+    void elaborarInforme() {
+    }
+
+    @Test
+    void invitar() {
+    }
+
+    @Test
+    void invitacionAceptada() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
